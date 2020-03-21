f3883f0571f60d7fd6348eb31da80650f015734f
zTzyrant <uhack91@gmail.com>
2020-03-21 14:42:55 +0800

    Fix Some Bug After Add Login User

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
   def create
        @user = User.find_by(username: params[:username])
         if @user && @user.authenticate(params[:password])
-          sessions[:user_id] = @user.id
-          edirect_to '/welcome'
+          session[:user_id] = @user.id
+          redirect_to '/welcome'
         else 
           redirect_to '/login'
         end
